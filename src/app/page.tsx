5bd0e2792ce84f92183c31fd5c98d144ea5f89d5
agocobrisha-afk <agocobrisha@gmail.com>
2026-07-22 11:41:40 -0700

    Switch homepage to complete red and white restaurant landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import FeaturedMenu from "@/components/FeaturedMenu";
-import About from "@/components/About";
-import Reviews from "@/components/Reviews";
-import Footer from "@/components/Footer";
-import WhatsAppFloat from "@/components/WhatsAppFloat";
+import PizzaHubLanding from "@/components/PizzaHubLanding";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <FeaturedMenu />
-        <About />
-        <Reviews />
-      </main>
-      <Footer />
-      <WhatsAppFloat />
-    </>
-  );
+  return <PizzaHubLanding />;
 }
