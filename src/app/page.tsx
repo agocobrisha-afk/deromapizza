06150c46a5f8cb16a86eec02556c17167257e020
agocobrisha-afk <agocobrisha@gmail.com>
2026-07-22 10:59:15 -0700

    Rebuild homepage with classic reference sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
-import FeaturedMenu from "@/components/FeaturedMenu";
-import Reviews from "@/components/Reviews";
+import ClassicRestaurantSections from "@/components/ClassicRestaurantSections";
 import Footer from "@/components/Footer";
 import WhatsAppFloat from "@/components/WhatsAppFloat";
 
@@ -12,18 +10,14 @@ export const fetchCache = "force-no-store";
 
 export default function Home() {
   return (
-    <>
-      <div className="min-h-screen bg-[#171717] text-white">
-        <Header />
-        <main>
-          <Hero />
-          <About />
-          <FeaturedMenu />
-          <Reviews />
-        </main>
-        <Footer />
-        <WhatsAppFloat />
-      </div>
-    </>
+    <div className="min-h-screen bg-[#171717] text-white">
+      <Header />
+      <main>
+        <Hero />
+        <ClassicRestaurantSections />
+      </main>
+      <Footer />
+      <WhatsAppFloat />
+    </div>
   );
 }
